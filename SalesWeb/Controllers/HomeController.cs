a9cceed9872257bbad0dc598ca38d7206bbca01f
Marco Antonio Pandolfo <marcopandolfo577@gmail.com>
2019-07-10 21:15:06 -0300

    First controller and Razor pages tests

--- SalesWeb/Controllers/HomeController.cs
+++ SalesWeb/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWeb.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Web MVC App from C# Course";
+            ViewData["Email"] = "marcopandolfo577@gmail.com";
 
             return View();
         }
