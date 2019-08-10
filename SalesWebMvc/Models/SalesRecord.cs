b5c9a1b1f2e962115fba03288b649ecb654de922
marcopandolfo <marcopandolfo577@gmail.com>
2019-08-10 15:09:56 -0300

    Models

---
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -0,0 +1,13 @@
+﻿using SalesWebMvc.Models.Enums;
+using System;
+
+namespace SalesWebMvc.Models
+{
+    public class SalesRecord
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+        public SaleStatus Status { get; set; }
+    }
+}
---
+++ SalesWebMvc/Models/Seller.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SalesWebMvc.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public double BaseSalary { get; set; }
+    }
+}
