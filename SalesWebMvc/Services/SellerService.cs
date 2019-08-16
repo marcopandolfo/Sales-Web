78e1183ab15717c9632433a2334cf459b4988062
marcopandolfo <marcopandolfo577@gmail.com>
2019-08-15 22:14:08 -0300

    ForeignKey not null

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -22,6 +22,7 @@ namespace SalesWebMvc.Services
 
         public void Insert(Seller obj)
         {
+            obj.Department = _context.Department.First();
             _context.Seller.Add(obj);
             _context.SaveChanges();
         }
