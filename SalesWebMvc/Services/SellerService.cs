4d6a05fc243be8fe2abf9eab5603d3490b1e11d2
marcopandolfo <marcopandolfo577@gmail.com>
2019-08-15 22:04:50 -0300

    Creating Seller form

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -19,5 +19,11 @@ namespace SalesWebMvc.Services
         {
             return _context.Seller.ToList();
         }
+
+        public void Insert(Seller obj)
+        {
+            _context.Seller.Add(obj);
+            _context.SaveChanges();
+        }
     }
 }
